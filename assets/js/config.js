3a43c295413dc3c6777801def7caf0c86e7ebdcd
leiaadm7 <leia.donoso@inacapmail.cl>
2025-11-27 19:01:11 -0300

    avance

---
+++ assets/js/config.js
@@ -0,0 +1,3 @@
+export const API_URL = "http://127.0.0.1:8000/api";
+export const LOGIN_URL = "http://127.0.0.1:8000/api/token/";
+export const REFRESH_URL = "http://127.0.0.1:8000/api/token/refresh/";
